dc8a40ffa4d52d557b24525f698ba96d5886a782
ShigureChen <70284977+ShigureChen@users.noreply.github.com>
2020-11-13 11:55:50 +0800

    Complete basic layout in portrait and landscape

---
+++ app/src/main/java/edu/temple/webbrowserapp_2/BrowserControlFragment.java
@@ -0,0 +1,20 @@
+package edu.temple.webbrowserapp_2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BrowserControlFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_browser_control, container, false);
+        return view;
+    }
+}
