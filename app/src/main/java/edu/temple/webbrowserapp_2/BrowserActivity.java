d61ce6344bc01a99c2ecec970ae0ce7d21bcde52
ShigureChen <70284977+ShigureChen@users.noreply.github.com>
2020-11-14 02:16:22 +0800

    add "add new tab" message

--- app/src/main/java/edu/temple/webbrowserapp_2/BrowserActivity.java
+++ app/src/main/java/edu/temple/webbrowserapp_2/BrowserActivity.java
@@ -4,46 +4,100 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BrowserActivity extends AppCompatActivity implements ViewPagerFragment.ViewPagerFragmentListener, PageControlFragment.PageControlFragmentListener, BrowserControlFragment.BrowserControlFragmentListener, WebViewFragment.WebViewFragmentListener {
 
     PageControlFragment pcf;
     ViewPagerFragment vpf;
     BrowserControlFragment bcf;
-    WebViewFragment wbf;
+    PageListFragment plf;
+    TextView textView;
     FragmentManager fm;
     FragmentTransaction ft;
-    String pageTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int orientation = getResources().getConfiguration().orientation;
 
-        if(pageTitle != null)
-        {
-            this.setTitle(pageTitle);
-        }
-
+        textView = findViewById(R.id.textView);
+        textView.setText("Press + button to add a new tab");
+        textView.setTextSize(14);
         pcf = new PageControlFragment();
         vpf = new ViewPagerFragment();
         bcf = new BrowserControlFragment();
+        plf = new PageListFragment();
 
-        fm = getSupportFragmentManager();
-        ft = fm.beginTransaction();
-        ft.add(R.id.container_view_pager, vpf)
-        .add(R.id.container_browser_control, bcf)
-        .add(R.id.container_page_control, pcf)
-                .commit();
+        if(orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            fm = getSupportFragmentManager();
+            ft = fm.beginTransaction();
+            ft.replace(R.id.container_view_pager, vpf)
+                    .replace(R.id.container_browser_control, bcf)
+                    .replace(R.id.container_page_control, pcf)
+                    .commit();
+        }
+
+        else if(orientation == Configuration.ORIENTATION_LANDSCAPE)
+        {
+            fm = getSupportFragmentManager();
+            ft = fm.beginTransaction();
+            ft.replace(R.id.container_view_pager, vpf)
+                    .replace(R.id.container_browser_control, bcf)
+                    .replace(R.id.container_page_control, pcf)
+                    .replace(R.id.container_page_list, plf)
+                    .commit();
+        }
+
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
 
+        ft.addToBackStack("ViewPagerFragment");
 
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
+        {
+            fm = getSupportFragmentManager();
+            ft = fm.beginTransaction();
+            ft.add(R.id.container_view_pager, vpf)
+                    .add(R.id.container_browser_control, bcf)
+                    .add(R.id.container_page_control, pcf)
+                    .add(R.id.container_page_list, plf)
+                    .commit();
+        }
+        else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            fm = getSupportFragmentManager();
+            ft = fm.beginTransaction();
+            ft.add(R.id.container_view_pager, vpf)
+                    .add(R.id.container_browser_control, bcf)
+                    .add(R.id.container_page_control, pcf)
+                    .commit();
+        }
+    }
 
     @Override
     public void addPage() {
         vpf.addNewTab();
+        this.textView.setText("");
     }
 
     //WebView to PageControl
